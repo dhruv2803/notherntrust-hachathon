f79282ce4afa54c867640773846c7cb8467a16a8
Rishi <rishishrimal2001@gmail.com>
2021-08-31 15:59:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const func = require('./function.js')
+
+
+app.get('/home', (req, res) => {
+    const {symbol} = req.body.symbol
+    res.send()
+})
+
+app.listen(8080,() => {
+    console.log("App Starting")
+})
